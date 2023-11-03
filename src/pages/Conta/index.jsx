81792c085d09100988ca2ac59635b11c940e77f2
Thadeu <thadeu1111@gmail.com>
2023-11-03 16:48:43 -0300

    adicionando a pagina conta nas rotas

--- src/pages/Conta/index.jsx
+++ src/pages/Conta/index.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import 'style.css'
+import { useForm } from "react-hook-form";
+import Header from "../../components/Header";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import "./style.css";
 
-export default function Conta() {
+const validationPost = yup.object().shape({
+  nome: yup.string().required("Preencha seu nome").max(100, "Até 100 caract."),
+  email: yup.string().required("Preencha seu email").max(100, "Até 100 caract."),
+  senha: yup.string().required("Preencha sua senha").max(20, "Até 20 caract."),
+  cpf: yup.string().required("Preencha seu CPF").max(11, "Até 11 caract."),
+  cep: yup.string().required("Preencha seu CEP(só numeros)").max(8, "Até 8 caract."),
+  complemento: yup.string().required("Preencha o complemento").max(100, "Até 100 caract."),
+  numero: yup.string().required("Preencha o numero da casa").max(5, "Até 5 caract.")
+});
+
+export default function Posts() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors } } = useForm({ resolver: yupResolver(validationPost) });
+
+  const addPost = (data) => console.log(data);
   return (
     <div>
-        
+      <Header />
+
+      <main>
+
+      </main>
     </div>
-  )
+  );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Conta from '../pages/Conta'
+import Veiculos from '../pages/Veiculos'
 
 function AppRouter() {
   return (
     <Routes>
         <Route path='/' element={<Home/>}> </Route>
-        <Route path='/veiculos' element={<Veículos/>}> </Route>
+        <Route path='/veiculos' element={<Veiculos/>}> </Route>
+        <Route path='/conta' element={<Conta/>}> </Route>
     </Routes>
   )
 }
