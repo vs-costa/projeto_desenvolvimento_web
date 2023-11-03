e9a064ff76058d91ca9c132f2b7fdae92f5476c6
Patricia <patriciastarck@gmail.com>
2023-11-03 20:53:32 -0300

    Criação da sessão Sobre

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import Conta from '../pages/Conta'
+import Sobre from '../pages/Sobre'
 import Veiculos from '../pages/Veiculos'
 
 function AppRouter() {
@@ -11,8 +12,10 @@ function AppRouter() {
         <Route path='/' element={<Home />}> </Route>
         <Route path='/veiculos' element={<Veiculos />}> </Route>
         <Route path='/conta' element={<Conta />}> </Route>
+        <Route path="/sobre" element={<Sobre />}></Route>
       </Routes>
     </div>
   )
 }
 export default AppRouter
+ 
