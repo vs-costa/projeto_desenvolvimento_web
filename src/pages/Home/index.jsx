349588b99922c547e2d6dde60ca73ffdc914fe09
Pedro Horacio <129102505+Pedroh88@users.noreply.github.com>
2023-11-06 22:20:23 -0300

    Pagina Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from 'react';
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import './style.css';
+import banner01 from '../../assets/banner01.png'
+import banner02 from '../../assets/banner02.png'
+import banner03 from '../../assets/banner03.png'
 
-export default function Home() {
-    return (
-        <div>
-            <Header/>
-            <h1>Pagina principal da empresa</h1>
-            <Footer />
+function Carrossel() {
+
+  return (
+    <div className='home' >
+      <Header/>
+      <div className="carrossel">
+        <div className="carrossel-inner">
+          <div className="carrossel-item active">
+            <img src={banner01} alt="Slide 1" />
+          </div>
+          <div className="carrossel-item">
+            <img src={banner02} alt="Slide 2" />
+          </div>
+          <div className="carrossel-item">
+            <img src={banner03} alt="Slide 3" />
+          </div>
         </div>
-    )
-}
+      </div>
+      <Footer/>
+    </div>
+  );
+};
+
+export default Carrossel;
