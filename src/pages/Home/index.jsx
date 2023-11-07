33de96490591c56a22835a5951712f76823b3f36
Pedro Horacio <129102505+Pedroh88@users.noreply.github.com>
2023-11-07 17:38:41 -0300

    Direcionamento de banners para pág. correspondente

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,22 +7,30 @@ import banner02 from '../../assets/banner02.png'
 import banner03 from '../../assets/banner03.png'
 import {Carousel} from 'react-responsive-carousel' 
 import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Link } from 'react-router-dom';
 
 function Carrossel() {
 
   return (
+
     <div className='home' >
       <Header/>
       <Carousel showThumbs={false} autoPlay={true} infiniteLoop={true} stopOnHover={true}>
-      <div>
-        <img src={banner01} alt="Imagem 1"  style={{width: '1000px'}}/>
-      </div>
-      <div>
-        <img src={banner02} alt="Imagem 2" style={{width: '1000px'}}/>
-      </div>
-      <div>
-        <img src={banner03} alt="Imagem 3" style={{width: '1000px'}} />
-      </div>
+ <Link to={"/nossoServico"}> 
+          <div>
+            <img src={banner01} alt="Imagem 1" style={{ width: '1000px' }} />
+          </div>
+      </Link>
+      <Link to={"/veiculos"}>
+          <div>
+            <img src={banner02} alt="Imagem 2" style={{ width: '1000px' }} />
+          </div>
+        </Link>
+        <Link to={"/sobre"}>
+          <div>
+            <img src={banner03} alt="Imagem 3" style={{ width: '1000px' }} />
+          </div>
+        </Link>
     </Carousel>
        <Footer/>
     </div>
